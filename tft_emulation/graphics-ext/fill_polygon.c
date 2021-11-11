ae339ebbd3b512aad8c50afe2427f0b7cc1a27df
Aleksander <alekader@rambler.ru>
2021-11-11 02:47:40 +0200

    2021-11-11

--- tft_emulation/graphics-ext/fill_polygon.c
+++ tft_emulation/graphics-ext/fill_polygon.c
@@ -3,16 +3,50 @@
 
 void fill_regular_polygon(int center_x, int center_y, int vertices, int radius, uint32_t color)
 {
+  if (vertices < 3 || radius < 1) return;
+
+  const double a = 6.28318530718 / (double)vertices;
+  int i = 1;
+  int y, px, py, nx, ny;
+
+  nx = +(int)(0.5 + radius * sin(a * (double)i));
+  ny = -(int)(0.5 + radius * cos(a * (double)i));
+
+  px = 0;
+  py = y = -radius;
+  while (y <= ny || ny > py)
+  {
+    const int x = px + (nx - px) * (y - py) / (ny - py);
+    setCursorX(center_x - x);
+    setCursorY(center_y + y);
+    drawLine(getCursorX(), getCursorY(), center_x + x, center_y + y, color);
+    y++;
+    while (y > ny)
+    {
+      if (nx < 0) return;
+      i++;
+      px = nx;
+      py = ny;
+      nx = +(int)(0.5 + radius * sin(a * (double)i));
+      ny = -(int)(0.5 + radius * cos(a * (double)i));
+    }
+  }
+}
+
+/**
+void fill_regular_polygon(int center_x, int center_y, int vertices, int radius, uint32_t color)
+{
+  if (vertices < 3 || radius < 1)
+  return;
+
   const double a = 2.0 * 3.14159265358979323846 / (double)vertices;
   int i = 1;
   int y, px, py, nx, ny;
-  if (vertices < 3 || radius < 1)
-  return;
-  px = 0;
-  py = -radius;
+
   nx = (int)(0.5 + radius * sin(a));
   ny = (int)(0.5 - radius * cos(a));
-  y = -radius;
+  px = 0;
+  py = y = -radius;
   while (y <= ny || ny > py)
   {
     const int x = px + (nx - px) * (y - py) / (ny - py);
@@ -30,12 +64,10 @@ void fill_regular_polygon(int center_x, int center_y, int vertices, int radius, 
       }
       if (center_x + x < Display_Get_WIDTH())
       {
-        // lineto(center_x + x, center_y + y);
         drawLine(getCursorX(), getCursorY(), center_x + x, center_y + y, color);
       }
       else
       {
-        // lineto(Display_Get_WIDTH() - 1, center_y + y);
         drawLine(getCursorX(), getCursorY(), Display_Get_WIDTH() - 1, center_y + y, color);
       }
     }
@@ -52,3 +84,4 @@ void fill_regular_polygon(int center_x, int center_y, int vertices, int radius, 
     }
   }
 }
+**/
